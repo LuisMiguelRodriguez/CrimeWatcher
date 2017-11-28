9091f9f6cbb4511eeb4a971bd96354f55a535599
Kapoor <okapoor@SDGL159ca7e23.local>
2017-11-27 17:01:55 -0800

    Added a few additional methods for analytics

--- client/src/components/Analytics/Analytics.js
+++ client/src/components/Analytics/Analytics.js
@@ -18,7 +18,19 @@ class Analytics extends Component {
   };
 
   componentDidMount() {
+//     var tmp = [{"bookdate":"2015-06-11T00:00:00.000","charge1":"PROBATION WARRANT","chargecode1":"32234002","defendant":"SAUNDERS, TAWANDA","dob":"1976-07-19T00:00:00.000","location_1_address":"852 NW 8TH STREET","location_1_city":"FTLAUDERDALE","location_1_state":"FL"}
+// ,{"bookdate":"2015-06-12T00:00:00.000","charge1":"GRAND THEFT 3RD DEG","chargecode1":"81201402C","defendant":"ROSSO, JORGE  ALBERTO","dob":"1956-01-09T00:00:00.000","location_1_address":"FITZ ROY 1450, BUENOS AI 9A","location_1_city":"AT","location_1_state":"BA","location_1_zip":"01414"}
+// ,{"bookdate":"2015-06-13T00:00:00.000","charge1":"BATTERY","chargecode1":"7840300","defendant":"BUTLER, BIANCA  KATHLIA","dob":"1988-03-11T00:00:00.000","location_1_address":"DIGNITY GARDENS 17 NASSA","location_1_city":"NASSAU","location_1_state":"BA","location_1_zip":"00000"}
+// ,{"bookdate":"2015-06-13T00:00:00.000","charge1":"BATTERY","chargecode1":"7840300","defendant":"HAIDER, ABDULLAH","dob":"1985-11-07T00:00:00.000","location_1_address":"BAYAN BLK 2 ST HOUSE 4-8 2","location_1_city":"MINA","location_1_state":"KU"}
+// ,{"bookdate":"2015-06-19T00:00:00.000","charge1":"PETIT THEFT 1D","chargecode1":"81201402E","defendant":"SANCHEZ-RUIZ, PERLA  MELINA","dob":"1996-08-07T00:00:00.000","location_1_address":"CANAL CUATRO Y SANCHO DE N38-","location_1_city":"QUITO","location_1_state":"EU"}
+// ,{"bookdate":"2015-06-19T00:00:00.000","charge1":"GRAND THEFT 3RD DEG","charge2":"ID/USE/POSS/FRAUD","charge3":"ID/USE/POSS/FRAUD","chargecode1":"81201402C","chargecode2":"81756802A","chargecode3":"81756802A","defendant":"KNOWLES, PERCIUS  ASHLEY","dob":"1985-07-22T00:00:00.000","location_1_address":"18 HILLARY AVE","location_1_city":"FREEPORT","location_1_state":"BH"}];
+//   Nvd3Analytics.day(tmp).then((res) => {
+//     console.log(res)
+//   });
+
   }
+
+  
   loadData = () => {
 
   };
--- client/src/utils/Nvd3Analytics.js
+++ client/src/utils/Nvd3Analytics.js
@@ -43,7 +43,68 @@ export default {
 			});
 			resolve(dayCounts);
 		})
-	}
+	},
+
+	age: (dataArray) => {
+		return new Promise ((resolve, reject) => {
+			let ageCounts = {};
+			ageCounts["age"] = [];
+			ageCounts["counts"] = [];
+			dataArray.forEach( (element) => {
+				let birthYear = 0;
+				if (element.dob) {
+					birthYear = parseInt(element.dob.split("-")[0]);
+				};
+
+				let currentAge = (2017-birthYear).toString();
+
+				if (ageCounts.age.indexOf(currentAge) === -1) {
+					ageCounts.age.push(currentAge);
+					ageCounts.counts.push(1)
+				} else {
+					ageCounts.counts[ageCounts.age.indexOf(currentAge)]++;
+				}
+			});
+			
+
+			let maxAge = Math.max(...ageCounts.age);
+			let minAge = Math.min(...ageCounts.age);
+			let maxCrimeAge = ageCounts.age[ageCounts.counts.indexOf(Math.max(...ageCounts.counts))];
+
+			console.log(maxAge + " " + minAge + " " + maxCrimeAge)
+
+			resolve({maxAge: maxAge,minAge: minAge,maxCrimeAge: maxCrimeAge})
+		})
+	},
+
+	day: (dataArray) => {
+		return new Promise ((resolve, reject) => {
+			let dayCounts = {};
+			dayCounts["day"] = [];
+			dayCounts["counts"] = [];
+			dataArray.forEach( (element) => {
+				let bookDate= element.bookdate;
+				let bookDay = bookDate.split("T")[0];
+
+				if (dayCounts.day.indexOf(bookDay) === -1) {
+					dayCounts.day.push(bookDay);
+					dayCounts.counts.push(1)
+				} else {
+					dayCounts.counts[dayCounts.day.indexOf(bookDay)]++;
+				}
+			});
+			
+			console.log(dayCounts)
+			let maxCrimeDay = dayCounts.day[dayCounts.counts.indexOf(Math.max(...dayCounts.counts))];
+			let minCrimeDay = dayCounts.day[dayCounts.counts.indexOf(Math.min(...dayCounts.counts))];
+			let maxCrimeDayCount = Math.max(...dayCounts.counts)
+			let minCrimeDayCount = Math.min(...dayCounts.counts)
+
+			resolve({maxCrimeDay: maxCrimeDay,minCrimeDay: minCrimeDay,maxCrimeDayCount: maxCrimeDayCount,minCrimeDayCount:minCrimeDayCount})
+		})
+	},
+
+	
 
 
 }
