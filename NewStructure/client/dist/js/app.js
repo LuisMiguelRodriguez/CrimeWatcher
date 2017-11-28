d41204aa0c2256328e57c8f108d289bd543e8a40
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2017-11-27 17:00:51 -0800

    add background pattern and centered form on login and signup

--- NewStructure/client/dist/js/app.js
+++ NewStructure/client/dist/js/app.js
@@ -48213,64 +48213,72 @@
 	      successMessage = _ref.successMessage,
 	      user = _ref.user;
 	  return _react2.default.createElement(
-	    _Card.Card,
-	    { className: 'container' },
+	    'div',
+	    { className: 'mainBackground' },
 	    _react2.default.createElement(
-	      'form',
-	      { action: '/', onSubmit: onSubmit },
-	      _react2.default.createElement(
-	        'h2',
-	        { className: 'card-heading' },
-	        'Login'
-	      ),
-	      successMessage && _react2.default.createElement(
-	        'p',
-	        { className: 'success-message' },
-	        successMessage
-	      ),
-	      errors.summary && _react2.default.createElement(
-	        'p',
-	        { className: 'error-message' },
-	        errors.summary
-	      ),
+	      _Card.Card,
+	      { className: 'container ' },
 	      _react2.default.createElement(
 	        'div',
-	        { className: 'field-line' },
-	        _react2.default.createElement(_TextField2.default, {
-	          floatingLabelText: 'Email',
-	          name: 'email',
-	          errorText: errors.email,
-	          onChange: onChange,
-	          value: user.email
-	        })
-	      ),
-	      _react2.default.createElement(
-	        'div',
-	        { className: 'field-line' },
-	        _react2.default.createElement(_TextField2.default, {
-	          floatingLabelText: 'Password',
-	          type: 'password',
-	          name: 'password',
-	          onChange: onChange,
-	          errorText: errors.password,
-	          value: user.password
-	        })
-	      ),
-	      _react2.default.createElement(
-	        'div',
-	        { className: 'button-line' },
-	        _react2.default.createElement(_RaisedButton2.default, { type: 'submit', label: 'Log in', primary: true })
-	      ),
-	      _react2.default.createElement(
-	        _Card.CardText,
-	        null,
-	        'Don\'t have an account? ',
+	        { className: 'card-container' },
 	        _react2.default.createElement(
-	          _reactRouter.Link,
-	          { to: '/signup' },
-	          'Create one'
-	        ),
-	        '.'
+	          'form',
+	          { action: '/', onSubmit: onSubmit },
+	          _react2.default.createElement(
+	            'h2',
+	            { className: 'card-heading' },
+	            'Login'
+	          ),
+	          successMessage && _react2.default.createElement(
+	            'p',
+	            { className: 'success-message' },
+	            successMessage
+	          ),
+	          errors.summary && _react2.default.createElement(
+	            'p',
+	            { className: 'error-message' },
+	            errors.summary
+	          ),
+	          _react2.default.createElement(
+	            'div',
+	            { className: 'field-line' },
+	            _react2.default.createElement(_TextField2.default, {
+	              floatingLabelText: 'Email',
+	              name: 'email',
+	              errorText: errors.email,
+	              onChange: onChange,
+	              value: user.email
+	            })
+	          ),
+	          _react2.default.createElement(
+	            'div',
+	            { className: 'field-line' },
+	            _react2.default.createElement(_TextField2.default, {
+	              floatingLabelText: 'Password',
+	              type: 'password',
+	              name: 'password',
+	              onChange: onChange,
+	              errorText: errors.password,
+	              value: user.password
+	            })
+	          ),
+	          _react2.default.createElement(
+	            'div',
+	            { className: 'button-line' },
+	            _react2.default.createElement(_RaisedButton2.default, { type: 'submit', label: 'Log in', primary: true })
+	          ),
+	          _react2.default.createElement(
+	            _Card.CardText,
+	            null,
+	            'Don\'t have an account? ',
+	            _react2.default.createElement(
+	              _reactRouter.Link,
+	              { to: '/signup' },
+	              'Create one'
+	            ),
+	            '.'
+	          )
+	        )
 	      )
 	    )
 	  );
@@ -48970,68 +48978,76 @@
 	      errors = _ref.errors,
 	      user = _ref.user;
 	  return _react2.default.createElement(
-	    _Card.Card,
-	    { className: 'container' },
+	    'div',
+	    { className: 'mainBackground' },
 	    _react2.default.createElement(
-	      'form',
-	      { action: '/', onSubmit: onSubmit },
-	      _react2.default.createElement(
-	        'h2',
-	        { className: 'card-heading' },
-	        'Sign Up'
-	      ),
-	      errors.summary && _react2.default.createElement(
-	        'p',
-	        { className: 'error-message' },
-	        errors.summary
-	      ),
+	      _Card.Card,
+	      { className: 'container' },
 	      _react2.default.createElement(
 	        'div',
-	        { className: 'field-line' },
-	        _react2.default.createElement(_TextField2.default, {
-	          floatingLabelText: 'Name',
-	          name: 'name',
-	          errorText: errors.name,
-	          onChange: onChange,
-	          value: user.name
-	        })
-	      ),
-	      _react2.default.createElement(
-	        'div',
-	        { className: 'field-line' },
-	        _react2.default.createElement(_TextField2.default, {
-	          floatingLabelText: 'Email',
-	          name: 'email',
-	          errorText: errors.email,
-	          onChange: onChange,
-	          value: user.email
-	        })
-	      ),
-	      _react2.default.createElement(
-	        'div',
-	        { className: 'field-line' },
-	        _react2.default.createElement(_TextField2.default, {
-	          floatingLabelText: 'Password',
-	          type: 'password',
-	          name: 'password',
-	          onChange: onChange,
-	          errorText: errors.password,
-	          value: user.password
-	        })
-	      ),
-	      _react2.default.createElement(
-	        'div',
-	        { className: 'button-line' },
-	        _react2.default.createElement(_RaisedButton2.default, { type: 'submit', label: 'Create New Account', primary: true })
-	      ),
-	      _react2.default.createElement(
-	        _Card.CardText,
-	        null,
-	        'Already have an account? ',
+	        { className: 'card-container' },
 	        _react2.default.createElement(
-	          _reactRouter.Link,
-	          { to: '/login' },
-	          'Log in'
+	          'form',
+	          { action: '/', onSubmit: onSubmit },
+	          _react2.default.createElement(
+	            'h2',
+	            { className: 'card-heading' },
+	            'Sign Up'
+	          ),
+	          errors.summary && _react2.default.createElement(
+	            'p',
+	            { className: 'error-message' },
+	            errors.summary
+	          ),
+	          _react2.default.createElement(
+	            'div',
+	            { className: 'field-line' },
+	            _react2.default.createElement(_TextField2.default, {
+	              floatingLabelText: 'Name',
+	              name: 'name',
+	              errorText: errors.name,
+	              onChange: onChange,
+	              value: user.name
+	            })
+	          ),
+	          _react2.default.createElement(
+	            'div',
+	            { className: 'field-line' },
+	            _react2.default.createElement(_TextField2.default, {
+	              floatingLabelText: 'Email',
+	              name: 'email',
+	              errorText: errors.email,
+	              onChange: onChange,
+	              value: user.email
+	            })
+	          ),
+	          _react2.default.createElement(
+	            'div',
+	            { className: 'field-line' },
+	            _react2.default.createElement(_TextField2.default, {
+	              floatingLabelText: 'Password',
+	              type: 'password',
+	              name: 'password',
+	              onChange: onChange,
+	              errorText: errors.password,
+	              value: user.password
+	            })
+	          ),
+	          _react2.default.createElement(
+	            'div',
+	            { className: 'button-line' },
+	            _react2.default.createElement(_RaisedButton2.default, { type: 'submit', label: 'Create New Account', primary: true })
+	          ),
+	          _react2.default.createElement(
+	            _Card.CardText,
+	            null,
+	            'Already have an account? ',
+	            _react2.default.createElement(
+	              _reactRouter.Link,
+	              { to: '/login' },
+	              'Log in'
+	            )
+	          )
 	        )
 	      )
 	    )
