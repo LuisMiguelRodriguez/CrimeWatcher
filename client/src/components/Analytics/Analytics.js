b75a932f1f62047a18cb100f99be6c09ec727bca
Om Kapoor <asha.4720@gmail.com>
2017-12-06 22:25:36 -0800

    style updates to black white and red

--- client/src/components/Analytics/Analytics.js
+++ client/src/components/Analytics/Analytics.js
@@ -477,7 +477,7 @@ class Analytics extends Component {
 
             <div className="row">
               <div className="col-md-12 text-center">
-                <button type="button" className="btn btn-danger btn-lg " onClick={this.callTheAPI}>Initiate Analytics</button>
+                <button type="button" className="buttonAnalytics" onClick={this.callTheAPI}>Initiate Analytics</button>
               </div>
             </div>
           </div>
