561fab8e5b553782ec5919abec7ad5f1bf8d3292
Jenmadev <info@maknowsthat.com>
2017-12-07 19:55:38 -0800

    updated the about page

--- client/src/containers/AboutPage.js
+++ client/src/containers/AboutPage.js
@@ -12,7 +12,7 @@ const AboutPage = () => (
         <h2 className="aboutheading" id="aboutheading">Meet the Creators</h2>
         </div>
       </div>
-      <div className="row">
+      <div className="row biorow">
         <div className="col-md-2">
           <img className="bioPictures" src="/img/bioPics/jen.jpg"/> 
           <div className="container">
@@ -21,72 +21,87 @@ const AboutPage = () => (
                 <a href="https://www.linkedin.com/in/ecomjenma/"><i className="fa fa-linkedin socialicon" aria-hidden="true"></i></a>
               </div>
               <div className="col-xs-3 col-md-3">
-                <a href="https://www.linkedin.com/in/ecomjenma/"><i className="fa fa-github-square socialicon" aria-hidden="true"></i></a>
+                <a href="https://www.linkedin.com/in/ecomjenma/"><i className="fa fa-github-square socialgit" aria-hidden="true"></i></a>
               </div>
             </div>
           </div>
         </div>
         <div className="col-md-8 col-md-offset-2">
-          <h2>Project Manager/FrontEnd Designer</h2>
-          <h3>Jen</h3>
-          <p> A UC Riverside alumni, Jen, project manager by birth and raised in the O.C. If you need a project done she is the one we call to get things done. She loves to try out different food locales with her boyfriend. She loves ice cream, gummy bears,
-            or just straight sugar. Oh did we mention she loves raccoons?
+          <h4>Project Manager/FrontEnd Designer</h4>
+          <h4>Jen</h4>
+          <p> She is a freelance marketing strategist based in Southern California who specializes in eCommerce and entrepreneurship. She's passionate about helping businesses finding untapped resources in their communication plans.
           </p>
         </div>
       </div>
-      <div className="row">
+      <div className="row biorow">
         <div className="col-md-2">
-          <img className="bioPictures" src="/img/bioPics/luis.png"/>
-          <div className="row">
-            <div className="col-xs-4 col-md-4">
-            <a href="https://www.linkedin.com/in/luisthecoder"><i className="fa fa-linkedin socialicon" aria-hidden="true"></i></a>
-            </div>
-            <div className="col-xs-3 col-md-3">
-                <a href="https://www.linkedin.com/in/ecomjenma/"><i className="fa fa-github-square socialicon" aria-hidden="true"></i></a>
+          <img className="bioPictures" src="/img/bioPics/luis.png"/> 
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-3 col-md-3">
+                <a href="https://www.linkedin.com/in/luisthecoder/" target="_blank"><i className="fa fa-linkedin socialicon" aria-hidden="true"></i></a>
+              </div>
+              <div className="col-xs-3 col-md-3">
+                <a href="https://github.com/LuisMiguelRodriguez" target="_blank"><i className="fa fa-github-square socialgit" aria-hidden="true"></i></a>
+              </div>
             </div>
           </div>
         </div>
         <div className="col-md-8 col-md-offset-2">
-          <h2>Front/Back End Developer</h2>
-          <h3>Luis</h3>
+          <h4>Front/Back End Developer</h4>
+          <h4>Luis</h4>
           <p>Luis loves Pizza and Cider. Born and raised in New York but was forced subcumb to the forces of the west side. But as he would say "Why not ask me in person about me?.....I am serious."
           </p>
         </div>
       </div>
-      <div className="row">
+      <div className="row biorow">
         <div className="col-md-2">
-          <img className="bioPictures" src="/img/bioPics/jen.jpg"/> 
-          <div className="row">
-            <div className="col-xs-4 col-md-4">
-              <a href="https://www.linkedin.com/in/ecomjenma/"><i className="fa fa-linkedin socialicon" aria-hidden="true"></i></a>
+          <img className="bioPictures" src="/img/bioPics/Om.png"/> 
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-3 col-md-3">
+                <a href="https://www.linkedin.com/in/omkapoor" target="_blank"><i className="fa fa-linkedin socialicon" aria-hidden="true"></i></a>
+              </div>
+              <div className="col-xs-3 col-md-3">
+                <a href="https://github.com/okapoor" target="_blank"><i className="fa fa-github-square socialgit" aria-hidden="true"></i></a>
+              </div>
             </div>
           </div>
         </div>
         <div className="col-md-8 col-md-offset-2">
-          <h2>Project Manager/FrontEnd Designer</h2>
-          <h3>Jen</h3>
-          <p> A UC Riverside alumni, Jen, project manager by birth and raised in the O.C. If you need a project done she is the one we call to get things done. She loves to try out different food locales with her boyfriend. She loves ice cream, gummy bears,
-            or just straight sugar. Oh did we mention she loves raccoons?
-          </p>
+          <h4>Back End Developer</h4>
+          <h4>Om</h4>
+          <p>DevOps engineer with a passion for solving problems and learning new technologies</p>
         </div>
       </div>
-      <div className="row">
+      <div className="row biorow">
         <div className="col-md-2">
-          <img className="bioPictures" src="/img/bioPics/jen.jpg"/> 
-          <div className="row">
-            <div className="col-xs-4 col-md-4">
-              <a href="https://www.linkedin.com/in/ecomjenma/"><i className="fa fa-linkedin socialicon" aria-hidden="true"></i></a>
+          <img className="bioPictures" src="/img/bioPics/petersheadshot.jpg"/> 
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-3 col-md-3">
+                <a href="https://www.linkedin.com/in/omkapoor" target="_blank"><i className="fa fa-linkedin socialicon" aria-hidden="true"></i></a>
+              </div>
+              <div className="col-xs-3 col-md-3">
+                <a href="https://github.com/okapoor" target="_blank"><i className="fa fa-github-square socialgit" aria-hidden="true"></i></a>
+              </div>
             </div>
           </div>
         </div>
         <div className="col-md-8 col-md-offset-2">
-          <h2>Project Manager/FrontEnd Designer</h2>
-          <h3>Jen</h3>
-          <p> A UC Riverside alumni, Jen, project manager by birth and raised in the O.C. If you need a project done she is the one we call to get things done. She loves to try out different food locales with her boyfriend. She loves ice cream, gummy bears,
-            or just straight sugar. Oh did we mention she loves raccoons?
-          </p>
+          <h4>Back End Developer/Copywriter</h4>
+          <h4>Om</h4>
+          <p>DevOps engineer with a passion for solving problems and learning new technologies</p>
         </div>
       </div>
+
+
+
+
+
+
+
+
     </div>
   </div>
 );
